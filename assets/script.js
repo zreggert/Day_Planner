d25bdc3806e94ff1955864048a8fc08b1d716a48
elgalloloco <zreggert@gmail.com>
2021-03-18 14:26:06 -0400

    fixed all saved buttons so they save to the appropriate time block and as well as stored the description in their time block and create get funcitions so those stored items can be retrieved even after the page is refreshed

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,69 @@
 var today = moment();
 var currentTime = moment().hours();
-
 $("#currentDay").text(moment().format('LLLL'));
-
+var timeBlock;
+var task;
 $(document).ready(function() {
     //setting items to local storage
+    
     $('#9 .btn').click(function() {
-        localStorage.setItem($("#9"), $('.description').val());
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
+        console.log(timeBlock)
     })
     $('#10 .btn').click(function() {
-        localStorage.setItem($("#10"), $('.description').val());
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
     })
-    $('.btn').click(function() {
-        localStorage.setItem($("#11"), $('.description').val());
+    $('#11 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
     })
-    $('.btn').click(function() {
-        localStorage.setItem($("#12"), $('.description').val());
+    $('#12 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
+    })
+    $('#13 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
+    })
+    $('#14 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
+    })
+    $('#15 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
+    })
+    $('#16 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
+    })
+    $('#17 .btn').click(function() {
+        var timeBlock = $(this).parent().attr('id') 
+        var task = $(this).siblings('.description').val()
+        localStorage.setItem(timeBlock, task);
     })
 
-    
-    $('#9 .description').val(localStorage.getItem($("#9"), $('.description').val()))
+
+    //getting from local storage
+    $('#9 .description').val(localStorage.getItem("9"));
+    $('#10 .description').val(localStorage.getItem("10"));
+    $('#11 .description').val(localStorage.getItem("11"));
+    $('#12 .description').val(localStorage.getItem("12"));
+    $('#13 .description').val(localStorage.getItem("13"));
+    $('#14 .description').val(localStorage.getItem("14"));
+    $('#15 .description').val(localStorage.getItem("15"));
+    $('#16 .description').val(localStorage.getItem("16"));
+    $('#17 .description').val(localStorage.getItem("17"));
 })
+
+

dcc599bad6900ff3eb6305d22de4ef44a79e6227
elgalloloco <zreggert@gmail.com>
2021-03-18 12:53:36 -0400

    created click functions to save the descriptions to local storage. however im pretty sure this is repetative code and there is a more streamline way to loop through the divs and use one function for all buttons rather than creating a function for each button. currently all my buttons are saving what is in the first time block as well because its going top down and my class are not specific to each button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,23 @@
 var today = moment();
+var currentTime = moment().hours();
 
 $("#currentDay").text(moment().format('LLLL'));
+
+$(document).ready(function() {
+    //setting items to local storage
+    $('#9 .btn').click(function() {
+        localStorage.setItem($("#9"), $('.description').val());
+    })
+    $('#10 .btn').click(function() {
+        localStorage.setItem($("#10"), $('.description').val());
+    })
+    $('.btn').click(function() {
+        localStorage.setItem($("#11"), $('.description').val());
+    })
+    $('.btn').click(function() {
+        localStorage.setItem($("#12"), $('.description').val());
+    })
+
+    
+    $('#9 .description').val(localStorage.getItem($("#9"), $('.description').val()))
+})

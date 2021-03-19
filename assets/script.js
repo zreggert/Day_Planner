86dd4b92299949cc4f43da557b5d143e4ecf52fc
elgalloloco <zreggert@gmail.com>
2021-03-19 15:29:38 -0400

    used a foreach loop in js to loop through my .row divs and compare their id to the current time to determine whether the div was past present or future. i added a data set because to compare the val of the id to the currenttime i was turning my id to just a number. so the data set was added to reidentify the div's id. currently i am trying to find a better what to get the values from local storage with a for loop rather than having a to use a line of code for each time block

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 var today = moment();
 var currentTime = moment().hours();
+//console.log(currentTime);
 $("#currentDay").text(moment().format('LLLL'));
 var timeBlock;
 var task;
@@ -10,60 +11,45 @@ $(document).ready(function() {
         var timeBlock = $(this).parent().attr('id') 
         var task = $(this).siblings('.description').val()
         localStorage.setItem(timeBlock, task);
-        console.log(timeBlock)
-    })
-    // $('#10 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#11 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#12 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#13 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#14 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#15 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#16 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-    // $('#17 .btn').click(function() {
-    //     var timeBlock = $(this).parent().attr('id') 
-    //     var task = $(this).siblings('.description').val()
-    //     localStorage.setItem(timeBlock, task);
-    // })
-
+    });
 
     //getting from local storage
-    $('#9 .description').val(localStorage.getItem("9"));
-    $('#10 .description').val(localStorage.getItem("10"));
-    $('#11 .description').val(localStorage.getItem("11"));
-    $('#12 .description').val(localStorage.getItem("12"));
-    $('#13 .description').val(localStorage.getItem("13"));
-    $('#14 .description').val(localStorage.getItem("14"));
-    $('#15 .description').val(localStorage.getItem("15"));
-    $('#16 .description').val(localStorage.getItem("16"));
-    $('#17 .description').val(localStorage.getItem("17"));
+    // $('#9 .description').val(localStorage.getItem("9"));
+    // $('#10 .description').val(localStorage.getItem("10"));
+    // $('#11 .description').val(localStorage.getItem("11"));
+    // $('#12 .description').val(localStorage.getItem("12"));
+    // $('#13 .description').val(localStorage.getItem("13"));
+    // $('#14 .description').val(localStorage.getItem("14"));
+    // $('#15 .description').val(localStorage.getItem("15"));
+    // $('#16 .description').val(localStorage.getItem("16"));
+    // $('#17 .description').val(localStorage.getItem("17"));
+
+    function getStoredTasks() {
+        var timeBlocks = document.querySelectorAll('.row');
+        timeBlocks.forEach(element => {
+            var hour = $(element).attr('id');
+            $('.description').val(localStorage.getItem(hour))
+        })
+    }
+
+
+    //changing the colors of the time blocks over time
+    function hoursLeft() {
+        var timeBlocks = document.querySelectorAll('.row');
+        //console.log(timeBlocks);
+        timeBlocks.forEach(element => {
+            var hour = $(element).attr('id');
+            var hourT = $(element).attr('data-hour');
+            //console.log(hour);
+            console.log(hourT);
+            if (hour > currentTime) {
+                $(hourT).addClass('future');
+            } else if (hour < currentTime) {
+                $(hourT).addClass('past');
+            } else {
+                $(hourT).addClass('present');
+            }
+         })
+    }
+    hoursLeft();
 })
-
-
